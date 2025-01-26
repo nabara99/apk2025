01c6aefa10b948b19ac777e0f460ae50a0d20c5a
nabara99 <rtaufiq212@gmail.com>
2025-01-26 09:08:52 +0700

    first-commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2025 <div class="bullet"></div> Created By <a href="https://nauval.in/">Ellypsis
+        </a>
+    </div>
+    <div class="footer-right">
+        version 2.1
+    </div>
+</footer>
